86fdfc3d9d4d905c861fbccea14c068b432f34eb
timelesnesses <timelessnesses@timelessnesses.me>
2024-08-12 07:22:44 +0700

    opencl

---
+++ opencl/fibs.cl
@@ -0,0 +1,85 @@
+/**
+  * No memoization sadly
+ */
+
+int fib_st_normal_real(int n) {
+    if (n == 0 || n == 1) {
+        return n;
+    }
+    return fib_st_normal_real(n - 1) + fib_st_normal_real(n - 2);
+}
+
+kernel void fib_st_normal(global ulong* results, ulong n) {
+    int id = get_global_id(0);
+    if (id < n) {
+        results[id] = fib_st_normal_real(n);
+    }
+}
+
+kernel void fib_st_linear(global ulong* results, ulong n) {
+    int id = get_global_id(0);
+
+    if (id < n) {
+        ulong a = 0;
+        ulong b = 1;
+        ulong next;
+
+        ulong m = id;
+        while (m > 0) {
+            m -= 1;
+            next = a + b;
+            a = b;
+            b = next;
+        }
+        results[id] = a;
+    }
+}
+
+typedef struct {
+    ulong a, b, c, d;
+} Matrix2x2;
+
+Matrix2x2 matrix_mul(Matrix2x2 m1, Matrix2x2 m2) {
+    Matrix2x2 res;
+    res.a = m1.a * m2.a + m1.b * m2.c;
+    res.b = m1.a * m2.b + m1.b * m2.d;
+    res.c = m1.c * m2.a + m1.d * m2.c;
+    res.d = m1.c * m2.b + m1.d * m2.d;
+    return res;
+}
+
+kernel void fib_st_matrix(global ulong* results, ulong n) {
+    int id = get_global_id(0);
+
+    if (id < n) {
+        Matrix2x2 step = {0, 1, 1, 1};
+        Matrix2x2 fib = {0, 1, 1, 1};
+
+        ulong m = id;
+        while (m > 0) {
+            m -= 1;
+            fib = matrix_mul(fib, step);
+        }
+        results[id] = fib.a;
+    }
+}
+
+kernel void fib_st_matrix_expo(global ulong* results, ulong n) {
+    int id = get_global_id(0);
+
+    if (id < n) {
+        Matrix2x2 fib = {1, 0, 0, 1};
+        Matrix2x2 step = {1, 1, 1, 0};
+
+        ulong m = id;
+        m -= 1;
+        while (m > 0) {
+            if (m & 1) {
+                fib = matrix_mul(fib, step);
+            }
+            step = matrix_mul(step, step);
+            m >>= 1;
+        }
+        results[id] = fib.a;
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,19 @@
+use ocl::ProQue;
 use rayon::iter::IntoParallelRefIterator;
 use rayon::iter::ParallelIterator;
 use std::ops::Mul;
 use std::{collections::HashMap, io::Write};
-
 fn main() {
-    let mut funcs: HashMap<&'static str, fn(u128) -> u128> = HashMap::new();
+    let mut funcs: HashMap<&'static str, fn(u64) -> u64> = HashMap::new();
     funcs.insert("test_singlthreaded_normal", fib_st_normal);
     funcs.insert("test_singlethreaded_memo", fib_st_memo);
     funcs.insert("test_singlethreaded_linear", fib_st_linear);
     funcs.insert("test_singlethreaded_matrix", fib_st_matrix);
     funcs.insert("test_singlethreaded_matrix_expo", fib_st_matrix_expo);
+    funcs.insert("test_gpu_normal", fib_normal_gpu_wrapper);
+    funcs.insert("test_gpu_linear", fib_linear_gpu_wrapper);
+    funcs.insert("test_gpu_matrix", fib_matrix_gpu_wrapper);
+    funcs.insert("test_gpu_matrix_expo", fib_matrix_expo_gpu_wrapper);
 
     rayon::ThreadPoolBuilder::new()
         .num_threads(funcs.len())
@@ -19,7 +23,7 @@ fn main() {
     let a = funcs
         .par_iter()
         .map(|(k, v)| (k.to_string(), test(v, k)))
-        .collect::<Vec<(String, u128)>>();
+        .collect::<Vec<(String, u64)>>();
 
     for x in a {
         println!(
@@ -30,8 +34,8 @@ fn main() {
 }
 
 #[must_use]
-fn test<T>(call: &dyn Fn(u128) -> T, test_name: &str) -> u128 {
-    let mut x = 0;
+fn test<T>(call: &dyn Fn(u64) -> T, test_name: &str) -> u64 {
+    let mut x = 1;
     println!("\nTesting {}", test_name);
     loop {
         let now = std::time::Instant::now();
@@ -39,7 +43,6 @@ fn test<T>(call: &dyn Fn(u128) -> T, test_name: &str) -> u128 {
         let ex_time = std::time::Instant::now();
         let a = (ex_time - now).as_secs_f32();
         if a > 1.0 {
-            // changing this to 0.5 so i dont have to wait a day
             break;
         }
         if (x % 100000) == 0 {
@@ -55,7 +58,47 @@ fn test<T>(call: &dyn Fn(u128) -> T, test_name: &str) -> u128 {
     x
 }
 
-fn fib_st_normal(n: u128) -> u128 {
+fn fib_normal_gpu_wrapper(i: u64) -> u64 {
+    let pro_que = ProQue::builder().src(include_str!("../opencl/fibs.cl")).dims(i as u32).build().expect("Failed to build proque");
+    let results = pro_que.create_buffer::<u64>().expect("Failed to build result buffer");
+    let kernel = pro_que.kernel_builder("fib_st_normal").arg(&results).arg(i).build().expect("Failed to build kernel");
+    unsafe { kernel.enq().expect("Failed to execute"); };
+    let mut b = vec![0u64;1];
+    results.read(&mut b).enq().expect("Failed to read");
+    b[0]
+}
+
+fn fib_linear_gpu_wrapper(i: u64) -> u64 {
+    let pro_que = ProQue::builder().src(include_str!("../opencl/fibs.cl")).dims(i as u32).build().expect("Failed to build proque");
+    let results = pro_que.create_buffer::<u64>().expect("Failed to build result buffer");
+    let kernel = pro_que.kernel_builder("fib_st_linear").arg(&results).arg(i).build().expect("Failed to build kernel");
+    unsafe { kernel.enq().expect("Failed to execute"); };
+    let mut b = vec![0u64;1];
+    results.read(&mut b).enq().expect("Failed to read");
+    b[0]
+}
+
+fn fib_matrix_gpu_wrapper(i: u64) -> u64 {
+    let pro_que = ProQue::builder().src(include_str!("../opencl/fibs.cl")).dims(i as u32).build().expect("Failed to build proque");
+    let results = pro_que.create_buffer::<u64>().expect("Failed to build result buffer");
+    let kernel = pro_que.kernel_builder("fib_st_matrix").arg(&results).arg(i).build().expect("Failed to build kernel");
+    unsafe { kernel.enq().expect("Failed to execute"); };
+    let mut b = vec![0u64;1];
+    results.read(&mut b).enq().expect("Failed to read");
+    b[0]
+}
+
+fn fib_matrix_expo_gpu_wrapper(i: u64) -> u64 {
+    let pro_que = ProQue::builder().src(include_str!("../opencl/fibs.cl")).dims(i as u32).build().expect("Failed to build proque");
+    let results = pro_que.create_buffer::<u64>().expect("Failed to build result buffer");
+    let kernel = pro_que.kernel_builder("fib_st_matrix_expo").arg(&results).arg(i).build().expect("Failed to build kernel");
+    unsafe { kernel.enq().expect("Failed to execute"); };
+    let mut b = vec![0u64;1];
+    results.read(&mut b).enq().expect("Failed to read");
+    b[0]
+}
+
+fn fib_st_normal(n: u64) -> u64 {
     match n {
         0 | 1 => n,
         _ => fib_st_normal(n - 1) + fib_st_normal(n - 2),
@@ -63,12 +106,12 @@ fn fib_st_normal(n: u128) -> u128 {
 }
 
 lazy_static::lazy_static! {
-    static ref CACHE: std::sync::Mutex<std::collections::HashMap<u128, u128>> = {
+    static ref CACHE: std::sync::Mutex<std::collections::HashMap<u64, u64>> = {
         std::sync::Mutex::new(std::collections::HashMap::new())
     };
 }
 
-fn fib_st_memo(n: u128) -> u128 {
+fn fib_st_memo(n: u64) -> u64 {
     match n {
         0 | 1 => n,
         _ => *CACHE
@@ -79,7 +122,7 @@ fn fib_st_memo(n: u128) -> u128 {
     }
 }
 
-fn fib_st_linear(mut n: u128) -> u128 {
+fn fib_st_linear(mut n: u64) -> u64 {
     let mut a = 0;
     let mut b = 1;
     let mut next;
@@ -94,7 +137,7 @@ fn fib_st_linear(mut n: u128) -> u128 {
 }
 
 #[derive(Clone, Copy)]
-struct Matrix2x2(u128, u128, u128, u128);
+struct Matrix2x2(u64, u64, u64, u64);
 
 impl Mul for Matrix2x2 { // mulassign bad
     type Output = Matrix2x2;
@@ -107,7 +150,7 @@ impl Mul for Matrix2x2 { // mulassign bad
     }
 }
 
-fn fib_st_matrix(mut n: u128) -> u128 {
+fn fib_st_matrix(mut n: u64) -> u64 {
     if n == 0 {
         return 0;
     }
@@ -120,7 +163,7 @@ fn fib_st_matrix(mut n: u128) -> u128 {
     fib.0
 }
 
-fn fib_st_matrix_expo(mut n: u128) -> u128 {
+fn fib_st_matrix_expo(mut n: u64) -> u64 {
     if n == 0 {
         return 0;
     }
@@ -140,8 +183,8 @@ fn fib_st_matrix_expo(mut n: u128) -> u128 {
 mod tests {
     use crate::{fib_st_linear, fib_st_matrix, fib_st_matrix_expo, fib_st_memo, fib_st_normal};
 
-    const FIB: u128 = 40;
-    const TARGET_VALUE: u128 = 102334155;
+    const FIB: u64 = 40;
+    const TARGET_VALUE: u64 = 102334155;
 
     #[test]
     fn test_normal() {
